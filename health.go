2645d011aff995ebf4fd6f82278792b8e9d27bce
bla005 <36144055+bla005@users.noreply.github.com>
2020-05-05 15:09:14 -0700

    fplm

--- health.go
+++ health.go
@@ -15,7 +15,7 @@ func healthHandler(w http.ResponseWriter, r *http.Request) {
 	healthResp := &healthResponse{
 		Durationn: time.Now().Sub(startTime),
 	}
-	// if duration > timeout in config....x
+	// if duration > timeout in config....xx
 
 	if err := json.NewEncoder(w).Encode(healthResp); err != nil {
 		//caca
