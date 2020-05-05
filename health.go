c9266164549f7333015d0f10c009b3ce870ae453
bla005 <36144055+bla005@users.noreply.github.com>
2020-05-05 16:14:09 -0700

    first commit

--- health.go
+++ health.go
@@ -2,19 +2,33 @@ package service
 
 import (
 	"encoding/json"
+	"net"
 	"net/http"
 	"time"
 )
 
 type healthResponse struct {
 	Durationn time.Duration
+	Pula      int
+}
+
+func checkHealth() int {
+	conn, err := net.DialTimeout("tcp", "localhost:8088/health", 2*time.Second)
+	if err != nil {
+		return 0
+	}
+	defer conn.Close()
+	return 1
 }
 
 func healthHandler(w http.ResponseWriter, r *http.Request) {
 	startTime := time.Now()
+
 	healthResp := &healthResponse{
 		Durationn: time.Now().Sub(startTime),
+		Pula:      checkHealth(),
 	}
+
 	// if duration > timeout in config....xx
 
 	if err := json.NewEncoder(w).Encode(healthResp); err != nil {
