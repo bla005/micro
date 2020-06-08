ad671734d551b2161af526b76fc3bf0b9d4d53ed
bla005 <36144055+bla005@users.noreply.github.com>
2020-06-08 00:49:26 -0700

    lol

--- health.go
+++ health.go
@@ -3,16 +3,15 @@ package service
 import (
 	"encoding/json"
 	"net/http"
-	"time"
 )
 
 func (s *service) healthHandler(w http.ResponseWriter, r *http.Request) {
 	resp := struct {
-		Uptime time.Duration `json:"uptime"`
-		Check  string        `json:"check"`
+		Uptime string `json:"uptime"`
+		Check  string `json:"check"`
 	}{
-		Uptime: s.GetUptime(),
-		Check:  "OK",
+		Uptime: s.GetUptime().String(),
+		Check:  "ok",
 	}
 	if err := json.NewEncoder(w).Encode(&resp); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
